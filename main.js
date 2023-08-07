9892f014d5b4b8318f2dd3f0e1d41143976a32e8
Edmir Galvan <edmirgalvaz@gmail.com>
2023-08-07 10:51:47 -0600

    Burger menu created

--- main.js
+++ main.js
@@ -1,9 +1,25 @@
 const headerDrone = document.getElementById("headerDrone");
+const headerMenu = document.getElementById("headerMenu");
+const headerMobileMenu = document.getElementById("headerMobileMenu");
+
+let isOpenMenu = false;
 
 addEventListener("scroll", () => {
-  if (window.scrollY > 200) {
+  if (window.scrollY > 200 && window.screen.width > 600) {
     headerDrone.style.display = "none";
   } else {
     headerDrone.style.display = "block";
+  }
+});
+
+headerMenu.addEventListener("click", () => {
+  if (!isOpenMenu) {
+    isOpenMenu = true;
+    headerMobileMenu.classList.remove("disabled");
+    headerMobileMenu.classList.add("header--mobile-menu");
+  } else {
+    isOpenMenu = false;
+    headerMobileMenu.classList.add("disabled");
+    headerMobileMenu.classList.remove("header--mobile-menu");
   }
 });
